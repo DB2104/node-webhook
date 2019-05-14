0c754c4cffdf4e9d5e88dc388f12847c3088ab16
DB21 <jmds2115@gmail.com>
2019-05-14 11:07:02 +0530

    modified as per dialog flow Api

--- routes/weather.routes.js
+++ routes/weather.routes.js
@@ -3,23 +3,36 @@ const request = require('request');
 require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 router.get('/webhook', (req, res) => {
-	//var queryText = req.body.queryResult.queryText;
-	//var city = req.body.queryResult.parameters['geo-city'];
+	var queryText = req.body.queryResult.queryText;
+	var city = req.body.queryResult.parameters['geo-city'];
 	//console.log(queryText);
-	var city = 'Mumbai';
 
-	var options = {
-		method: 'GET',
-		uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-	};
-	request(options, (error, response, body) => {
-		if (error) res.status(400).json(error);
-		else {
-			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-			console.log('body:', body);
-			res.json(body);
-		}
-	});
+	if (city) {
+		var options = {
+			method: 'GET',
+			uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+		};
+		request(options, (error, response, body) => {
+			if (error) {
+				return res.json({
+					speech: `Unable to fetch weather for city - ${city}`,
+					displayText: `Unable to fetch weather for city - ${city}`
+				});
+			} else {
+				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+				console.log('body:', body);
+				return res.json({
+					speech: `Description : ${body.weather.description}`,
+					displayText: `Description : ${body.weather.description}`
+				});
+			}
+		});
+	} else {
+		return res.json({
+			speech: `Unable to fetch weather for city - ${city}`,
+			displayText: `Unable to fetch weather for city - ${city}`
+		});
+	}
 });
 
 module.exports = router;
